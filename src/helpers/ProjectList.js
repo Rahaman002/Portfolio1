1f2355a643e939fe953effd6f0b6ac832b8ea010
Rahaman <36rahaman@gmail.com>
2023-08-22 02:28:57 +0530

    first

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,17 @@
+import Proj1 from "../assets/proj1.jpg";
+import Proj2 from "../assets/proj2.png";
+
+export const ProjectList = [
+  {
+    name: "TODO-APP",
+    image: Proj1,
+    skills: "ReactJS,Bootstrap,Strapi",
+    link:"https://github.com/Rahaman002/Todo.git"
+  },
+  {
+    name: "Tic Tac Toe",
+    image: Proj2,
+    skills: "HTML,CSS,Javascript",
+    link:"https://github.com/Rahaman002/https-github.com-Rahaman002-timepass-projects-tree-master-Tic-tac-toe.git"
+  },
+];
